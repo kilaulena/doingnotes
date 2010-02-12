9ef889fc4b30b4284479a83298dc543e933876bb
Lena Herrmann <lena@zeromail.org>
2010-02-12 12:47:35 +0100

    Removing unneeded step definitions

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -12,25 +12,3 @@ Then /^I should see "([^\"]*)" before "([^\"]*)"$/ do |first, second|
     raise("#{first} can't be found before #{second}") 
   end
 end
-
-# Then /^I should see "([^\"]*)" twice$/ do |text|
-#   regexp = Regexp.new(text + "(.+)" + text)
-#   response.body.should contain(regexp)
-# end
-
-
-# Then /^"([^\"]*)" should have the class "([^\"]*)"$/ do |element_id, css_class|
-#   find_element(:text_field, element_id).attribute_value(:class).should include(css_class)
-# end
-# 
-# Then /^"([^\"]*)" should not have the class "([^\"]*)"$/ do |element_id, css_class|
-#   find_element(:text_field, element_id).attribute_value(:class).should_not include(css_class)
-# end
-
-def find_element(kind, attribute)
-  matchers = [[attribute, :id], [attribute, :name]]
-  matchers << [$browser.label(:text, attribute).for, :id] if $browser.label(:text, attribute).exist?
-  matchers.map{ |field, matcher| 
-    $browser.send(kind, matcher, field)
-  }.find(&:exist?) ||  raise("#{kind} '#{attribute}' not found")
-end
